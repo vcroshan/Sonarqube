663e6fee16edc1c6ad7ee446049724f4f3a296f7
Roshan <v.c.roshan@gmail.com>
2023-01-12 10:01:09 +0530

    Update userdata-amazonlinux2.sh

--- userdata-amazonlinux2.sh
+++ userdata-amazonlinux2.sh
@@ -17,3 +17,6 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo echo "vm.max_map_count = 262144" >> /etc/sysctl.d/99-sysctl.conf
 
 sudo systemctl restart docker
+
+sudo mkdir -p /opt/sonarqube/conf /opt/sonarqube/data /opt/sonarqube/extensions /opt/sonarqube/lib/bundled-plugins /opt/sonarqube/logs /var/lib/postgresql/data
+sudo chmod 777 -R /opt/sonarqube/conf /opt/sonarqube/data /opt/sonarqube/extensions /opt/sonarqube/lib/bundled-plugins /opt/sonarqube/logs /var/lib/postgresql/data
